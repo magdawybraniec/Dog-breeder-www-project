916e034b298d6799b9cb20a599cefd6c5630bc3f
magdawybraniec <magdalena.wybraniec@gmail.com>
2020-01-16 08:17:17 +0100

    Add courses shop
    Shop added, gallery and whole page polished

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var accordions = document.getElementsByClassName("accordion");
+let accordions = document.getElementsByClassName("accordion");
 
 for (var i = 0; i < accordions.length; i++) {
   accordions[i].onclick = function() {
     this.classList.toggle('is-open');
 
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
